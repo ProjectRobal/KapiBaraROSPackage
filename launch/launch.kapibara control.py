0df14fdc533a9863c59fdb82014c1a1c82da3316
ProjectRobal <patryk1-70@wp.pl>
2024-02-05 21:27:37 +0100

    First commit

---
+++ launch/launch.kapibara control.py
@@ -0,0 +1,92 @@
+import os
+import logging
+from ament_index_python.packages import get_package_share_directory,get_package_prefix
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription,TimerAction,RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.event_handlers import OnProcessStart
+
+from launch_ros.actions import Node
+import xacro
+
+from launch.actions import SetEnvironmentVariable
+
+def generate_launch_description():
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'kapibara'
+    file_subpath = 'description/kapibara.urdf.xacro'
+
+
+    # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+    # Configure the node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw,
+        'use_sim_time': False}] # add other parameters here if required
+    )
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+
+    controller_params_file = os.path.join(get_package_share_directory(pkg_name),'config','my_controllers.yaml')
+
+
+    state_publisher = Node(package='joint_state_publisher_gui', executable='joint_state_publisher_gui',
+                    arguments=[],
+                    output='screen')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{"roboto_description": robot_description},
+                    controller_params_file]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0,actions=[
+        controller_manager
+    ])
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    delayed_diff_drive_spawner= RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner]
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    delayed_joint_broad_spawner= RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner]
+        )
+    )
+
+  
+
+    # Run the node
+    return LaunchDescription([
+        node_robot_state_publisher,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+    ])
+
+
